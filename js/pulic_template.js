caa878015c42d8d634ecff36113c749d53462fc4
ddv <cgrddv@163.com>
2020-09-30 23:14:37 +0800

    ss

--- js/pulic_template.js
+++ js/pulic_template.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
     $('header').load('public-html/head.html');
     $('footer').load('public-html/foot.html');
+    $('#float-menu').load('public-html/float-menu.html');
 });
