fde8f8df862228e27a3a9d34fbf97ae250f088d3
ddv <cgrddv@163.com>
2020-10-07 17:20:37 +0800

    vue

--- js/pulic_template.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-    $('header').load('public-html/head.html');
-    $('footer').load('public-html/foot.html');
-    $('#float-menu').load('public-html/float-menu.html');
-});
